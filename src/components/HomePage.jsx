f5368b0855c0633adb42d00965129e88e4568867
ADARSH SINGH <9792051262a@gmail.com>
2024-05-14 15:53:35 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,11 +12,11 @@ const HomePage = () => {
     if (!authUser) {
       navigate("/login");
     }
-  }, []);
+  }, [authUser, navigate]);
   return (
     <div>
       <div className="flex items-center justify-center">
-        <img src={logo} alt="Image" className="w-10 h-08 mr-2" />
+        <img src={logo} alt="converseX logo" className="w-10 h-08 mr-2" />
         <h1 className='text-4xl text-center text-white mb-4'>converseX</h1>
       </div>
       <div className='flex sm:h-[500px] md:h-[550px] rounded-lg overflow-hidden bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
