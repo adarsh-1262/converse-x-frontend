ab89c57d105b3565f41af7acb9cac7f72b0599e6
ADARSH SINGH <9792051262a@gmail.com>
2024-05-14 15:59:44 +0530

    Update useGetMessages.jsx

--- src/hooks/useGetMessages.jsx
+++ src/hooks/useGetMessages.jsx
@@ -1,24 +1,28 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react';
 import axios from "axios";
-import {useSelector,useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { setMessages } from '../redux/messageSlice';
 import { BASE_URL } from '..';
 
 const useGetMessages = () => {
-    const {selectedUser} = useSelector(store=>store.user);
+    const { selectedUser } = useSelector(store => store.user);
     const dispatch = useDispatch();
+
     useEffect(() => {
         const fetchMessages = async () => {
             try {
                 axios.defaults.withCredentials = true;
                 const res = await axios.get(`${BASE_URL}/api/v1/message/${selectedUser?._id}`);
-                dispatch(setMessages(res.data))
+                dispatch(setMessages(res.data));
             } catch (error) {
                 console.log(error);
             }
+        };
+
+        if (selectedUser?._id) {
+            fetchMessages();
         }
-        fetchMessages();
-    }, [selectedUser?._id,setMessages]);
+    }, [selectedUser?._id, dispatch]);
 }
 
-export default useGetMessages
+export default useGetMessages;
